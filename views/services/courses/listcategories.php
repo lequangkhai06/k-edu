7ed978957ac5870c26826409b2c8e5b6176b6c02
lequangkhai06 <khaidev.com@gmail.com>
2024-05-21 19:13:21 +0700

    modified:   .htaccess 	modified:   model/categories/data.php 	modified:   views/services/courses/categories.php 	new file:   views/services/courses/listcategories.php

--- views/services/courses/listcategories.php
+++ views/services/courses/listcategories.php
@@ -2,22 +2,22 @@
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/handler/config.php');
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/layouts/head.php');
 require_once(realpath($_SERVER["DOCUMENT_ROOT"]) . '/layouts/nav.php');
-$slug = $_GET['slug'];
-$categories = $conn->query("SELECT * FROM categories WHERE slug = '{$slug}'", 1)->fetch_array();
+$categories = $conn->query("SELECT * FROM categories", 1)->fetch_array();
 if (!$categories) {
     die('<script>location.href="/home"</script>');
 }
-$get = $conn->query("SELECT * FROM webinars WHERE type = 'text_lesson' AND category_id = '{$categories['id']}' ORDER BY id DESC");
+$get = $conn->query("SELECT * FROM webinars WHERE type = 'text_lesson' ORDER BY id DESC");
 $count = $get->num_rows;
 ?>
 <form class="container mt-30">
-    <h2>Danh Mục: <?=$categories["title"];?></h2>
     <section class="mt-20">
         <div>
             <div id="topFilters" class="shadow-lg border border-gray300 rounded-sm p-10 p-md-20">
                 <div class="row align-items-center">
                     <div class="col-lg-6 d-flex align-items-center">
-                        <select id="sort" class="form-control font-14" onchange="fitler_course()"<?php if ($count==0) {echo 'disabled';} ?>>
+                        <select id="sort" class="form-control font-14" onchange="fitler_course()" <?php if ($count == 0) {
+                                                                                                        echo 'disabled';
+                                                                                                    } ?>>
                             <option disabled selected>Sắp xếp theo</option>
                             <option value="">Mặc định</option>
                             <option value="newest">Mới nhất</option>
@@ -27,7 +27,9 @@ $count = $get->num_rows;
                         </select>
                     </div>
                     <div class="col-lg-6 d-flex align-items-center">
-                        <select id="price" class="form-control font-14" onchange="fitler_course()"<?php if ($count ==0) {echo 'disabled';} ?>>
+                        <select id="price" class="form-control font-14" onchange="fitler_course()" <?php if ($count == 0) {
+                                                                                                        echo 'disabled';
+                                                                                                    } ?>>
                             <option disabled selected>Tìm theo giá</option>
                             <option value="0">Mặc định</option>
                             <option value="1">Từ 100,000đ trở xuống</option>
@@ -39,10 +41,10 @@ $count = $get->num_rows;
                     </div>
                 </div>
             </div>
-            <div class="row mt-20"> 
+            <div class="row mt-20">
                 <div class="col-12 col-lg-8">
-                    <div id="loading" class="mt-20"> 
-                        <div class="loading-icon"></div> 
+                    <div id="loading" class="mt-20">
+                        <div class="loading-icon"></div>
                     </div>
                     <div class="list_course">
                     </div>
@@ -51,10 +53,14 @@ $count = $get->num_rows;
                 <div class="col-12 col-lg-4">
                     <div class="mt-20 p-20 rounded-sm shadow-lg border border-gray300 filters-container">
                         <div class="input-group">
-                            <input type="search" id="char" onchange="fitler_course()" autocomplete="off" class="form-control rounded" placeholder="Tìm kiếm từ khóa..." <?php if ($count == 0) {echo 'disabled';} ?>/>
+                            <input type="search" id="char" onchange="fitler_course()" autocomplete="off" class="form-control rounded" placeholder="Tìm kiếm từ khóa..." <?php if ($count == 0) {
+                                                                                                                                                                            echo 'disabled';
+                                                                                                                                                                        } ?> />
                         </div>
 
-                        <button type="reset" onclick="clear_fitler()" class="btn btn-sm btn-primary btn-block mt-30" <?php if ($count == 0) {echo 'disabled';} ?>>Xóa lọc</button>
+                        <button type="reset" onclick="clear_fitler()" class="btn btn-sm btn-primary btn-block mt-30" <?php if ($count == 0) {
+                                                                                                                            echo 'disabled';
+                                                                                                                        } ?>>Xóa lọc</button>
                     </div>
                 </div>
             </div>
@@ -74,12 +80,12 @@ $count = $get->num_rows;
     char = "";
     price = "";
     sort = "";
-    slug = "<?php echo $slug; ?>";
+    slug = "";
     // tai khoa hoc
     function load_course() {
         $("#loading").show();
         $(".list_course").hide();
-        $.post("<?= $domain; ?>model/categories/data.php", {
+        $.post("<?= $domain; ?>model/categories/data.php?query=all", {
                 page: page,
                 char: char,
                 price: price,
@@ -98,8 +104,7 @@ $count = $get->num_rows;
                     style: {
                         background: "linear-gradient(to right, #00b09b, #96c93d)",
                     },
-                    onClick: function(){
-                    } // Callback after click
+                    onClick: function() {} // Callback after click
                 }).showToast();
                 $("#loading").hide();
                 $(".list_course").html('');
